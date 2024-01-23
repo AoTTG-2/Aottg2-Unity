4ab7ba3dcf59bdc17c81bf9d5a50a6302002fe8b
FizzySD <64667188+FizzySD@users.noreply.github.com>
2024-01-23 12:24:08 +0100

    Cleaning Code and unused classes

--- Assets/VoiceChatManager.cs
+++ Assets/VoiceChatManager.cs
@@ -56,13 +56,11 @@ public class VoiceChatManager : MonoBehaviourPunCallbacks
     {
         if(PV.IsSpeaking && !_keepTalking) 
         {
-            Debug.Log("A");
             RPCManager.PhotonView.RPC("EmoteVoiceRPC", RpcTarget.All, new object[] { character.Cache.PhotonView.ViewID, "Speaking"});
             _keepTalking = true;
         }
         else if(!PV.IsSpeaking && _keepTalking)
         {
-            Debug.Log("B");
             RPCManager.PhotonView.RPC("StopVoiceRPC", RpcTarget.All, new object[] {});
             _keepTalking = false;
         }
@@ -70,9 +68,7 @@ public class VoiceChatManager : MonoBehaviourPunCallbacks
 
     public override void OnPlayerEnteredRoom(Player player) 
     {
-        Debug.Log("test");
         ApplySoundSettings();
-
     }
     public void Apply(AudioSource Mic) 
     {
