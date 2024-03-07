dab8b8f6b0e4656e5ed25f25559bf49b67204f72
AutumnThyme <79295722+AutumnThyme@users.noreply.github.com>
2024-03-07 18:55:14 -0500

    Added previews and changed water post processing script to WaterEffect.cs and reserved PostProcessingManager.cs for future work

---
+++ Assets/Scripts/ApplicationManagers/PostProcessingManager.cs
@@ -0,0 +1,15 @@
+using ApplicationManagers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingManager : MonoBehaviour
+{
+    [SerializeField]GameObject PostProcessingVolume;
+
+    
+    void Start()
+    {
+
+    }
+}
