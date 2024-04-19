8936d56aad585801a72fcb8ef5456489c3ae1016
ZippyStew45 <38433129+ZippyStew45@users.noreply.github.com>
2024-04-18 22:10:56 -0400

    tried to fix spacing

--- Assets/Scripts/UI/ExpeditionPopup/ExpeditionPlayersPanel.cs
+++ Assets/Scripts/UI/ExpeditionPopup/ExpeditionPlayersPanel.cs
@@ -24,7 +24,7 @@ namespace UI
         {
             base.Setup(parent);
             ExpeditionPopup RolesPopup = (ExpeditionPopup)parent;
-            string cat = RolesPopup.LocaleCategory;
+            string cat = RolesPopup.LocaleCategory; //zippy: fix the null ref error
             string sub = "Players";
             EMSettings settings = SettingsManager.EMSettings;
 
@@ -35,7 +35,7 @@ namespace UI
             foreach (Player player in PhotonNetwork.PlayerList)
             {
                 string name = ChatManager.GetIDString(player.ActorNumber, player.IsMasterClient) + player.GetStringProperty(PlayerProperty.Name);
-                GameObject obj = ElementFactory.CreateDefaultButton(DoublePanelLeft, style, name, 400f, 35f,
+                GameObject obj = ElementFactory.CreateDefaultButton(DoublePanelLeft, style, name, 400f, 25f,
                                                                     onClick: () => OnPlayerButtonClick(player));
             }
 
@@ -43,50 +43,12 @@ namespace UI
 
             #region Right Side
 
-            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP All To Me", 400f, 35f, onClick: () => OnTPPlayerButtonClick(0));
-            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP Selected player to me", 400f, 35f, onClick: () => OnTPPlayerButtonClick(1));
-            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP Me to selected player", 400f, 35f, onClick: () => OnTPPlayerButtonClick(2));
-            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP Selected player to Coords", 400f, 35f, onClick: () => OnTPPlayerButtonClick(3));
+            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP All To Me", 400f, 20f, onClick: () => OnTPPlayerButtonClick(0));
+            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP Selected player to me", 400f, 20f, onClick: () => OnTPPlayerButtonClick(1));
+            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP Me to selected player", 400f, 20f, onClick: () => OnTPPlayerButtonClick(2));
+            ElementFactory.CreateDefaultButton(DoublePanelRight, style, "TP Selected player to Coords", 400f, 20f, onClick: () => OnTPPlayerButtonClick(3));
             ElementFactory.CreateInputSetting(DoublePanelRight, style, settings.TPcoords, "Coordinates", tooltip: "Coordinates for TP button", elementWidth: 100f); 
 
-            #endregion
-            #region //Code
-            /*
-            
-            ElementFactory.CreateDropdownSetting(DoublePanelLeft, style, settings.Language, "Language", UIManager.GetLanguages(),
-                elementWidth: 160f, onDropdownOptionSelect: () => settingsPopup.RebuildCategoryPanel(), tooltip: UIManager.GetLocaleCommon("RequireRestart"));
-            ElementFactory.CreateDropdownSetting(DoublePanelLeft, style, settings.CameraMode, UIManager.GetLocale(cat, sub, "CameraMode"),
-                 new string[] { "TPS", "Original" }, elementWidth: 200f);
-            ElementFactory.CreateSliderSetting(DoublePanelLeft, style, settings.CameraDistance, UIManager.GetLocale(cat, sub, "CameraDistance"),
-               elementWidth: 135f);
-            ElementFactory.CreateSliderSetting(DoublePanelLeft, style, settings.CameraHeight, UIManager.GetLocale(cat, sub, "CameraHeight"),
-               elementWidth: 135f);
-            ElementFactory.CreateSliderSetting(DoublePanelLeft, style, settings.CameraSide, UIManager.GetLocale(cat, sub, "CameraSide"),
-               elementWidth: 135f);
-            ElementFactory.CreateToggleSetting(DoublePanelLeft, style, settings.CameraTilt, UIManager.GetLocale(cat, sub, "CameraTilt"));
-            ElementFactory.CreateToggleSetting(DoublePanelLeft, style, settings.CameraClipping, UIManager.GetLocale(cat, sub, "CameraClipping"),
-                UIManager.GetLocale(cat, sub, "CameraClippingTooltip"));
-            ElementFactory.CreateInputSetting(DoublePanelLeft, style, settings.FOVMin, UIManager.GetLocale(cat, sub, "FOVMin"), tooltip:
-                UIManager.GetLocale(cat, sub, "FOVMinTooltip"), elementWidth: 100f);
-            ElementFactory.CreateInputSetting(DoublePanelLeft, style, settings.FOVMax, UIManager.GetLocale(cat, sub, "FOVMax"), tooltip:
-                UIManager.GetLocale(cat, sub, "FOVMaxTooltip"), elementWidth: 100f);
-            ElementFactory.CreateInputSetting(DoublePanelLeft, style, settings.FPSFOVMin, UIManager.GetLocale(cat, sub, "FPSFOVMin"), tooltip:
-                UIManager.GetLocale(cat, sub, "FPSFOVMinTooltip"), elementWidth: 100f);
-            ElementFactory.CreateInputSetting(DoublePanelLeft, style, settings.FPSFOVMax, UIManager.GetLocale(cat, sub, "FPSFOVMax"), tooltip:
-                UIManager.GetLocale(cat, sub, "FPSFOVMaxTooltip"), elementWidth: 100f);
-            ElementFactory.CreateSliderInputSetting(DoublePanelRight, new ElementStyle(titleWidth: 165f, themePanel: ThemePanel), settings.MouseSpeed, UIManager.GetLocale(cat, sub, "MouseSpeed"),
-              sliderWidth: 135f, decimalPlaces: 3);
-            ElementFactory.CreateToggleSetting(DoublePanelRight, style, settings.InvertMouse, UIManager.GetLocale(cat, sub, "InvertMouse"));
-            ElementFactory.CreateToggleSetting(DoublePanelRight, style, settings.MinimapEnabled, UIManager.GetLocale(cat, sub, "MinimapEnabled"));
-            ElementFactory.CreateInputSetting(DoublePanelRight, style, settings.MinimapHeight, UIManager.GetLocale(cat, sub, "MinimapHeight"),
-                elementWidth: 100f);
-            ElementFactory.CreateToggleSetting(DoublePanelRight, style, settings.SnapshotsEnabled, UIManager.GetLocale(cat, sub, "SnapshotsEnabled"));
-            ElementFactory.CreateToggleSetting(DoublePanelRight, style, settings.SnapshotsShowInGame, UIManager.GetLocale(cat, sub, "SnapshotsShowInGame"));
-            ElementFactory.CreateInputSetting(DoublePanelRight, style, settings.SnapshotsMinimumDamage, UIManager.GetLocale(cat, sub, "SnapshotsMinimumDamage"),
-                elementWidth: 100f);
-            ElementFactory.CreateToggleSetting(DoublePanelRight, style, settings.SkipCutscenes, UIManager.GetLocale(cat, sub, "SkipCutscenes"));
-            
-             */
             #endregion
         }
 
