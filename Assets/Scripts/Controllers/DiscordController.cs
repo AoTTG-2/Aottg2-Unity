9f435f65cefb1202a3b9029dbeb7051dab000fad
Rinus Van Linden <rinus.vanlinden@gmail.com>
2024-06-06 23:19:20 +0200

    fixed script so i can display name at the start and not when in a game

--- Assets/Scripts/Controllers/DiscordController.cs
+++ Assets/Scripts/Controllers/DiscordController.cs
@@ -6,9 +6,10 @@ using Settings;
 using Photon.Pun;
 using GameManagers;
 
-public class DiscordController : MonoBehaviour {
+public class DiscordController : MonoBehaviour
+{
 
-	public Discord.Discord discord;
+    public Discord.Discord discord;
 
     private static bool instanceExists;
     private string largeImage = "aottg2-logo1";
@@ -17,115 +18,75 @@ public class DiscordController : MonoBehaviour {
     private void Awake()
     {
         if (!instanceExists)
-		{
-			instanceExists = true;
+        {
+            instanceExists = true;
             DontDestroyOnLoad(gameObject);
-		}
-		else if(FindObjectsOfType(GetType()).Length > 1) 
-		{
-			Destroy(gameObject);
-		}
+        }
+        else if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Use this for initialization
-    void Start () {
-        discord = new Discord.Discord(1247921316913483888, (UInt64)Discord.CreateFlags.Default);
+    void Start()
+    {
+        discord = new Discord.Discord(1247921316913483888, (UInt64)Discord.CreateFlags.NoRequireDiscord);
         time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
 
-        UpdateStatus(); 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		try
-		{
+        UpdateStatus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
             discord.RunCallbacks();
         }
-		catch
-		{
-			Destroy(gameObject);
-		}
-	}
+        catch
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void LateUpdate()
     {
-		UpdateStatus();
+        UpdateStatus();
     }
 
     void UpdateStatus()
-	{
-		try
-		{
-            try
-            {
-                player = GameObject.FindGameObjectWithTag("Player");
-            }
-            catch
-            {
-                Debug.Log("Player not found");
-                player = null;
-            }
+    {
+        try
+        {
             var activityManager = discord.GetActivityManager();
-            if(player != null)
+            var activity = new Discord.Activity
             {
-                var activity = new Discord.Activity
-                {
-                    Details = "Name: " + player.GetComponent<Human>().Name,
-                    State = "Playing Aottg2!",
-                    Assets =
+                Details = "Name: " + SettingsManager.ProfileSettings.Name.Value,
+                State = "Playing Aottg2!",
+                Assets =
                 {
                     LargeImage = largeImage,
                 },
-                    Timestamps =
+                Timestamps =
                 {
                     Start = time,
                 }
-
-
-
-                };
-                activityManager.UpdateActivity(activity, (res) =>
-                {
-                    if (res != Discord.Result.Ok)
-                    {
-                        Debug.Log("Discord is not connected!");
-                    }
-                });
-            }
-            else
+            };
+            activityManager.UpdateActivity(activity, (res) =>
             {
-                var activity = new Discord.Activity
+                if (res != Discord.Result.Ok)
                 {
-                    Details = "",
-                    State = "Playing Aottg2!",
-                    Assets =
-                {
-                    LargeImage = largeImage,
-                },
-                    Timestamps =
-                {
-                    Start = time,
+                    Debug.Log("Discord is not connected!");
                 }
-
-
-
-                };
-                activityManager.UpdateActivity(activity, (res) =>
-                {
-                    if (res != Discord.Result.Ok)
-                    {
-                        Debug.Log("Discord is not connected!");
-                    }
-                });
-            }
-            
+            });
         }
         catch
         {
             Debug.Log("Discord activity update Failed");
             Destroy(gameObject);
         }
-	}
+    }
 
     private void OnApplicationQuit()
     {
