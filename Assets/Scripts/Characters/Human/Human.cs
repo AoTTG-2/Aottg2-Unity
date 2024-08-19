52c64f38980d1c6972171bfcb3039635a0832431
NinfarZ <tassolima2@gmail.com>
2024-08-19 21:44:53 +0100

    fix air dashing during wall slide

--- Assets/Scripts/Characters/Human/Human.cs
+++ Assets/Scripts/Characters/Human/Human.cs
@@ -390,11 +390,20 @@ namespace Characters
                 Vector3 direction = GetTargetDirection();
                 _originalDashSpeed = Cache.Rigidbody.velocity.magnitude;
                 _targetRotation = GetTargetRotation();
-                Cache.Rigidbody.rotation = _targetRotation;
+                if(!_wallSlide)
+                {
+                    //The line below was causing problems when dashing away from walls at certain angles.
+                    //Removing it fixed that but I'm unsure if it's needed for another situation (didn't notice a difference without it), do uncomment if the case
+                    //Cache.Rigidbody.rotation = _targetRotation;
+                    CrossFade(HumanAnimations.Dash, 0.1f, 0.1f);
+                }
+                   
+                else
+                    PlayAnimation(HumanAnimations.Dodge, 0.2f);           
                 EffectSpawner.Spawn(EffectPrefabs.GasBurst, Cache.Transform.position, Cache.Transform.rotation);
                 PlaySound(HumanSounds.GasBurst);
                 _dashTimeLeft = 0.5f;
-                CrossFade(HumanAnimations.Dash, 0.1f, 0.1f);
+                
                 State = HumanState.AirDodge;
                 FalseAttack();
                 Cache.Rigidbody.AddForce(direction * 40f, ForceMode.VelocityChange);
