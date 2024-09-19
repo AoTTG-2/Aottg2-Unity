0ecbc690a83d93aa61f6f9b15b7bcdea6563a8bf
rc174945 <80923699+rc174945@users.noreply.github.com>
2024-09-19 14:12:18 -0500

    Remove fx vertex streams, fix map editor saving, fix achievment priority

--- Assets/Scripts/GameProgress/AchievementItem.cs
+++ Assets/Scripts/GameProgress/AchievementItem.cs
@@ -6,5 +6,17 @@ namespace GameProgress
     {
         public StringSetting Tier = new StringSetting(string.Empty);
         public BoolSetting Active = new BoolSetting(false);
+
+        public override string GetQuestName()
+        {
+            string tier = "A";
+            if (Tier.Value == "Bronze")
+                tier = "A";
+            else if (Tier.Value == "Silver")
+                tier = "B";
+            else if (Tier.Value == "Gold")
+                tier = "C";
+            return Category.Value + GetConditionsHash() + tier + Amount.Value.ToString();
+        }
     }
 }
