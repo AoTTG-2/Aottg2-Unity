1731d5cae056e077b505ca5d63b216704342f2c7
Ghegi Jimenez <dev@gisketch.com>
2024-09-27 00:32:32 +0800

    Add LeanTween, GisketchUI system setup, Button
    - add new LeanTween package
    - set up GisketchUI system
    - add GisketchButton

---
+++ Assets/Scripts/GisketchUI/GisketchColors.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace GisketchUI
+{
+    public static class GisketchColors
+    {
+        public static readonly Color Blue = new Color(0.1254902f, 0.3960784f, 0.627451f);    // #2065a0
+        public static readonly Color Orange = new Color(0.7294118f, 0.4f, 0.1215686f);       // #ba661f
+        public static readonly Color Green = new Color(0.1607843f, 0.5333334f, 0.5411765f);  // #29888a
+        public static readonly Color Red = new Color(0.5058824f, 0.2392157f, 0.3215686f);    // #813d52
+        public static readonly Color Purple = new Color(0.3803922f, 0.2980392f, 0.5647059f); // #614c90
+
+        public static readonly Color Primary = Red;
+        public static readonly Color Secondary = Purple;
+        public static readonly Color Tertiary = Blue;
+    }
+}
---
+++ Assets/Scripts/GisketchUI/GisketchUIAnimator.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GisketchUI
+{
+    public static class GisketchUIAnimator
+    {
+        public static void HoverAnimation(RectTransform rectTransform, Vector3 originalPosition, float offset, float duration)
+        {
+            Vector3 hoverPosition = originalPosition + new Vector3(offset, offset, 0);
+            LeanTween.cancel(rectTransform);
+            LeanTween.move(rectTransform, hoverPosition, duration).setEaseOutQuint();
+        }
+
+        public static void UnhoverAnimation(RectTransform rectTransform, Vector3 originalPosition, float duration)
+        {
+            LeanTween.cancel(rectTransform);
+            LeanTween.move(rectTransform, originalPosition, duration).setEaseOutQuint();
+        }
+
+        public static void PressAnimation(RectTransform rectTransform, Vector3 originalPosition, float offset, float duration)
+        {
+            Vector3 pressPosition = originalPosition - new Vector3(offset, offset, 0);
+            LeanTween.cancel(rectTransform);
+            LeanTween.move(rectTransform, pressPosition, duration).setEaseOutQuint();
+        }
+
+        public static void ReleaseAnimation(RectTransform rectTransform, Vector3 originalPosition, float hoverOffset, float duration)
+        {
+            Vector3 hoverPosition = originalPosition + new Vector3(hoverOffset, hoverOffset, 0);
+            LeanTween.cancel(rectTransform);
+            LeanTween.move(rectTransform, hoverPosition, duration).setEaseOutQuint();
+        }
+
+        public static void ChangeButtonState(Image baseImage, Image outlineImage, Text labelText, Sprite newSprite, Color outlineColor, Color textColor)
+        {
+            baseImage.sprite = newSprite;
+            outlineImage.color = outlineColor;
+            labelText.color = textColor;
+        }
+    }
+}
