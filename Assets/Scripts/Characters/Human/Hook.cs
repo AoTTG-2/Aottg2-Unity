a704ebb29fe0b453ad0e90bd074f83740d710787
FizzySD2 <67657483+FizzySD2@users.noreply.github.com>
2024-10-09 14:10:10 +0200

    Debugging BounceOnOneHook

--- Assets/Scripts/Characters/Human/Hook.cs
+++ Assets/Scripts/Characters/Human/Hook.cs
@@ -327,7 +327,7 @@ namespace Characters
                             {
                                 MapObject mapObject = MapLoader.GoToMapObject[obj.transform.root.gameObject];
                                 if (mapObject.ScriptObject.Static)
-                                    SetHooked(finalHit.point + new Vector3(0f, 0.03f, 0f));
+                                    SetHooked(finalHit.point + new Vector3(0f, 0.1f, 0f));
                                 else
                                     SetHooked(finalHit.point, obj.transform, -1, mapObject.ScriptObject.Id);
                                 var collisionHandler = mapObject.GameObject.GetComponent<CustomLogicCollisionHandler>();
