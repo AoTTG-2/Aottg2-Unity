87543635ff6ae89fed8826c598fa940f489814c3
oryxoik <oryxoik@gmail.com>
2024-10-22 21:57:52 +0500

    Evaluator

---
+++ Assets/Scripts/CustomLogic/CustomLogicExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomLogic
+{
+    static class CustomLogicExtensions
+    {
+        public static bool IsExtension(this CustomLogicClassDefinitionAst classAst)
+        {
+            return (int)classAst.Token.Value == (int)CustomLogicSymbol.Extension;
+        }
+        
+        public static bool IsCutscene(this CustomLogicClassDefinitionAst classAst)
+        {
+            return (int)classAst.Token.Value == (int)CustomLogicSymbol.Cutscene;
+        }
+
+        public static bool TokenValueIsSymbol(this CustomLogicToken token, CustomLogicSymbol symbol)
+        {
+            return (int)token.Value == (int)symbol;
+        }
+    }
+}
