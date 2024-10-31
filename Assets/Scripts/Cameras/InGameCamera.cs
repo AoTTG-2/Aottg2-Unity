2c1bda23715d04026c46371916cea02f0962b110
Emre Aslan <emreaslan.gs@hotmail.com>
2024-10-31 17:24:26 +0300

    Lock nearest neck instead of titan

--- Assets/Scripts/Cameras/InGameCamera.cs
+++ Assets/Scripts/Cameras/InGameCamera.cs
@@ -509,7 +509,8 @@ namespace Cameras
             {
                 if (character is BaseTitan && !TeamInfo.SameTeam(character, _follow))
                 {
-                    float distance = Vector3.Distance(_follow.Cache.Transform.position, character.Cache.Transform.position);
+                    var neck = ((BaseTitan)character).BaseTitanCache.Neck;
+                    float distance = Vector3.Distance(_follow.Cache.Transform.position, neck.position);
                     if (distance < nearestDistance)
                     {
                         nearestDistance = distance;
