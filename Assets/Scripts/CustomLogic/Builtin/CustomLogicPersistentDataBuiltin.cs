782fdfe2b15d394df7867a1b7a828763eb39f15e
oryxoik <oryxoik@gmail.com>
2024-12-28 09:55:22 +0500

    PersistentData

--- Assets/Scripts/CustomLogic/Builtin/CustomLogicPersistentDataBuiltin.cs
+++ Assets/Scripts/CustomLogic/Builtin/CustomLogicPersistentDataBuiltin.cs
@@ -1,38 +1,34 @@
-﻿using ApplicationManagers;
-using GameManagers;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
 using Utility;
 using SimpleJSONFixed;
 using System.Linq;
 
 namespace CustomLogic
 {
-    [CLType(Static = true)]
+    [CLType(Static = true, Abstract = true)]
     class CustomLogicPersistentDataBuiltin : CustomLogicClassInstanceBuiltin
     {
         public CustomLogicPersistentDataBuiltin() : base("PersistentData")
         {
         }
 
-        [CLMethod("Sets a property in the persistent data.")]
+        [CLMethod("Sets the property with given name to the object value. Valid value types are float, string, bool, and int.")]
         public static void SetProperty(string property, object value)
         {
-            if (!(value == null || value is float || value is int || value is string || value is bool))
+            if (value is not (null or float or int or string or bool))
                 throw new System.Exception("PersistentData.SetProperty only supports null, float, int, string, or bool values.");
+            
             CustomLogicManager.PersistentData[property] = value;
         }
 
-        [CLMethod("Gets a property from the persistent data.")]
+        [CLMethod("Gets the property with given name. If property does not exist, returns defaultValue.")]
         public static object GetProperty(string property, object defaultValue)
         {
-            if (CustomLogicManager.PersistentData.ContainsKey(property))
-                return CustomLogicManager.PersistentData[property];
-            return defaultValue;
+            return CustomLogicManager.PersistentData.GetValueOrDefault(property, defaultValue);
         }
 
-        [CLMethod("Loads persistent data from a file.")]
+        [CLMethod("Loads persistent data from given file name. If encrypted is true, will treat the file as having been saved as encrypted.")]
         public static void LoadFromFile(string fileName, bool encrypted)
         {
             Directory.CreateDirectory(FolderPaths.PersistentData);
@@ -41,30 +37,30 @@ namespace CustomLogic
             if (!Util.IsValidFileName(fileName))
                 throw new System.Exception("PersistentData.LoadFromFile only supports legal fileName characters.");
 
-            string path = Path.Combine(FolderPaths.PersistentData, fileName + ".txt");
+            var path = Path.Combine(FolderPaths.PersistentData, fileName + ".txt");
             if (File.Exists(path))
             {
-                string text = File.ReadAllText(path);
+                var text = File.ReadAllText(path);
                 if (encrypted)
                     text = new SimpleAES().Decrypt(text);
                 var json = JSON.Parse(text);
-                foreach (string key in json.Keys)
+                foreach (var key in json.Keys)
                 {
-                    string value = json[key].Value;
-                    string type = value.Split(':')[0];
-                    if (type == "float")
-                        CustomLogicManager.PersistentData[key] = float.Parse(value.Substring(6));
-                    else if (type == "int")
-                        CustomLogicManager.PersistentData[key] = int.Parse(value.Substring(4));
-                    else if (type == "string")
-                        CustomLogicManager.PersistentData[key] = value.Substring(7);
-                    else if (type == "bool")
-                        CustomLogicManager.PersistentData[key] = value.Substring(5) == "1";
+                    var value = json[key].Value;
+                    var type = value.Split(':')[0];
+                    CustomLogicManager.PersistentData[key] = type switch
+                    {
+                        "float" => float.Parse(value[6..]),
+                        "int" => int.Parse(value[4..]),
+                        "string" => value[7..],
+                        "bool" => value[5..] == "1",
+                        _ => CustomLogicManager.PersistentData[key]
+                    };
                 }
             }
         }
 
-        [CLMethod("Saves persistent data to a file.")]
+        [CLMethod("Saves current persistent data to given file name. If encrypted is true, will also encrypt the file instead of using plaintext.")]
         public static void SaveToFile(string fileName, bool encrypted)
         {
             Directory.CreateDirectory(FolderPaths.PersistentData);
@@ -72,23 +68,23 @@ namespace CustomLogic
             if (!Util.IsValidFileName(fileName))
                 throw new System.Exception("PersistentData.LoadFromFile only supports legal fileName characters.");
 
-            string path = Path.Combine(FolderPaths.PersistentData, fileName + ".txt");
+            var path = Path.Combine(FolderPaths.PersistentData, fileName + ".txt");
             JSONNode node = new JSONObject();
-            foreach (string key in CustomLogicManager.PersistentData.Keys)
+            foreach (var key in CustomLogicManager.PersistentData.Keys)
             {
-                object value = CustomLogicManager.PersistentData[key];
+                var value = CustomLogicManager.PersistentData[key];
                 if (value == null)
                     continue;
                 if (value is float)
-                    node.Add(key, new JSONString("float:" + value.ToString()));
+                    node.Add(key, new JSONString("float:" + value));
                 else if (value is int)
-                    node.Add(key, new JSONString("int:" + value.ToString()));
+                    node.Add(key, new JSONString("int:" + value));
                 else if (value is string)
-                    node.Add(key, new JSONString("string:" + value.ToString()));
-                else if (value is bool)
-                    node.Add(key, new JSONString("bool:" + (((bool)value == true) ? "1" : "0")));
+                    node.Add(key, new JSONString("string:" + value));
+                else if (value is bool b)
+                    node.Add(key, new JSONString("bool:" + (b ? "1" : "0")));
             }
-            string text = node.ToString(aIndent: 4);
+            var text = node.ToString(aIndent: 4);
             if (encrypted)
                 text = new SimpleAES().Encrypt(text);
             if (text.Length > (1000 * 1000))
@@ -98,19 +94,19 @@ namespace CustomLogic
             File.WriteAllText(path, text);
         }
 
-        [CLMethod("Clears all properties from the persistent data.")]
+        [CLMethod("Clears current persistent data.")]
         public static void Clear()
         {
             CustomLogicManager.PersistentData.Clear();
         }
 
-        [CLMethod("Checks if the specified file name is valid.")]
+        [CLMethod("Determines whether or not the given fileName will be allowed for use when saving/loading a file.")]
         public static bool IsValidFileName(string fileName)
         {
             return Util.IsValidFileName(fileName);
         }
 
-        [CLMethod("Checks if the specified file exists.")]
+        [CLMethod("Determines whether the file given already exists. Throws an error if given an invalid file name.")]
         public static bool FileExists(string fileName)
         {
             if (!Util.IsValidFileName(fileName))
