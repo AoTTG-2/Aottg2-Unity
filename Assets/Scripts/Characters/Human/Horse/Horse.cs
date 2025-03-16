ee2537c2831ee3098f8f9f2f408eae3ceadcbe26
hongyaohongyao <1207105685@qq.com>
2025-03-17 01:04:09 +0800

    update only horse animation if human acts.

--- Assets/Scripts/Characters/Human/Horse/Horse.cs
+++ Assets/Scripts/Characters/Human/Horse/Horse.cs
@@ -216,26 +216,27 @@ namespace Characters
             base.LateUpdate();
             if (IsMine())
             {
-                if (_owner == null || _owner.Dead || _owner.State != HumanState.Idle)
+                if (_owner == null || _owner.Dead)
                     return;
+                bool humanActing = _owner.State != HumanState.Idle;
                 if (Cache.Rigidbody.velocity.magnitude > 8f)
                 {
                     CrossFadeIfNotPlaying(HorseAnimations.Run, 0.1f);
-                    if (_owner.MountState == HumanMountState.Horse)
+                    if (_owner.MountState == HumanMountState.Horse && !humanActing)
                         _owner.CrossFadeIfNotPlaying(HumanAnimations.HorseRun, 0.1f);
                     _idleTimeLeft = 0f;
                 }
                 else if (Cache.Rigidbody.velocity.magnitude > 1f)
                 {
                     CrossFadeIfNotPlaying(HorseAnimations.Walk, 0.1f);
-                    if (_owner.MountState == HumanMountState.Horse)
+                    if (_owner.MountState == HumanMountState.Horse && !humanActing)
                         _owner.CrossFadeIfNotPlaying(HumanAnimations.HorseIdle, 0.1f);
                     _idleTimeLeft = 0f;
                 }
                 else
                 {
                     UpdateIdle();
-                    if (_owner.MountState == HumanMountState.Horse)
+                    if (_owner.MountState == HumanMountState.Horse && !humanActing)
                         _owner.CrossFadeIfNotPlaying(HumanAnimations.HorseIdle, 0.1f);
                 }
             }
