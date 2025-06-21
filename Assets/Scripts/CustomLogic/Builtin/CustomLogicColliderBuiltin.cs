7c6def4b81c9ae7295c2ac95e355581a6a305745
AutumnThyme <79295722+AutumnThyme@users.noreply.github.com>
2025-06-21 16:00:58 -0400

    Update CustomLogicColliderBuiltin.cs

--- Assets/Scripts/CustomLogic/Builtin/CustomLogicColliderBuiltin.cs
+++ Assets/Scripts/CustomLogic/Builtin/CustomLogicColliderBuiltin.cs
@@ -60,16 +60,16 @@ namespace CustomLogic
         [CLProperty]
         public int ExludeLayers
         {
-            get => collider.gameObject.layer;
-            set => collider.gameObject.layer = value;
+            get => collider.excludeLayers;
+            set => collider.excludeLayers = value;
         }
 
         /// <inheritdoc cref="Collider.includeLayers"/>
         [CLProperty]
         public int includeLayers
         {
-            get => collider.gameObject.layer;
-            set => collider.gameObject.layer = value;
+            get => collider.includeLayers;
+            set => collider.includeLayers = value;
         }
 
         /// <inheritdoc cref="Collider.isTrigger"/>
