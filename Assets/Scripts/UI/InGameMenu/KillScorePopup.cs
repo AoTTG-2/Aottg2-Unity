81a9f0fd712bcd13f0dc08e9f6d8a9e65d363acc
fjql <98269704+fjql@users.noreply.github.com>
2025-07-20 01:14:47 +0800

    Removed dot in kill score pop-up

--- Assets/Scripts/UI/InGameMenu/KillScorePopup.cs
+++ Assets/Scripts/UI/InGameMenu/KillScorePopup.cs
@@ -29,6 +29,7 @@ namespace UI
             base.Setup(parent);
             _scoreLabel = ElementFactory.InstantiateAndBind(transform, "Prefabs/InGame/KillScoreLabel").GetComponent<Text>();
             _backgroundLabel = _scoreLabel.transform.Find("BackgroundLabel").GetComponent<Text>();
+            _scoreLabel.transform.parent.Find("Border").GetComponent<Image>().enabled = false;
         }
 
         public void Show(int score)
